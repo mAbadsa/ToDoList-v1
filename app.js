3e41a8378b133325562b44ba646303d8cd1cdad7
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2019-09-24 17:26:34 +0300

    add html code and ejs

--- app.js
+++ app.js
@@ -7,21 +7,39 @@ const publicDirPath = path.join(__dirname, './public');
 const viewsDirPath = path.join(__dirname, './views');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const items = [];
+
+const { PORT = 3000 } = process.env;
 
 app.use(express.static(publicDirPath));
 app.set('views', viewsDirPath);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+
+	const toDay = new Date(),
+		option = {
+			weekday: 'long',
+			month: 'long',
+			day: 'numeric'
+		};
+	const day = toDay.toLocaleDateString('en-US', option);
+
 	res.render('index', {
-	title: 'ToDoList'
-	})
+		title: 'ToDoList',
+		kinOfDay: day,
+		newListItem: items
+	});
 })
 
+app.post('/', (req, res) => {
+	var item = req.body.newItem;
+	items.push(item);
+	res.redirect('/');
+})
 
-app.listen(port, () => {
-	console.log('Server is running on port', port);
+app.listen(PORT, () => {
+	console.log('Server is running on port', PORT);
 })
